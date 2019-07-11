703e7b1a6f06526207bafaa1858363fd8eed4e0a
TRheed <rhksghd8303@naver.com>
2019-07-11 14:30:19 +0900

    PelinNoise 그래프 출력

--- Scripts/Game/Game.cs
+++ Scripts/Game/Game.cs
@@ -42,7 +42,12 @@ namespace SFML_NET_3D
 
         private void Initialize()
         {
-            noiseFactors = new NoiseFactors(interval:10,randomSeed:new Random().Next(255));
+            noiseFactors = new NoiseFactors
+            (
+                interval: 10,
+                randomSeed: new Random().Next(255),
+                softness: 3
+            );
             noise = Noise(noiseFactors);
             Renderer.Clear();
 
@@ -96,7 +101,7 @@ namespace SFML_NET_3D
                     {
                         float dist = Distnace(new Vector2f(x + 0.5f, z + 0.5f), new Vector2f(boxCount.X / 2, boxCount.Z / 2));
                         float offset2 = Map(dist, 0, Distnace(new Vector2f(0, 0), new Vector2f(boxCount.X / 2, boxCount.Z / 2)), 0, 1000);
-                        float height = 180 + 90 * Map(noise[(int)(offset1+offset2)%noiseFactors.Size],GetMin(noise),GetMax(noise),-1,1);
+                        float height = 180 + 90 * Map(noise[(int)(offset1 + offset2) % noiseFactors.Size], GetMin(noise), GetMax(noise), -1, 1);
                         boxes[x, y, z].SetSize(new Vector3f(boxSize.X, height, boxSize.Y));
                     }
                 }
@@ -105,7 +110,7 @@ namespace SFML_NET_3D
             foreach (var box in boxList)
                 box.Update();
 
-            if (offset1 <= noiseFactors.Size - noiseFactors.Interval*1.01f)
+            if (offset1 <= noiseFactors.Size - noiseFactors.Interval * 1.01f)
                 offset1 += noiseFactors.Interval;
             else
                 offset1 = 0;
@@ -114,6 +119,23 @@ namespace SFML_NET_3D
         private void Display()
         {
             Renderer.Render();
+
+            for (int i = 1; i < noiseFactors.Size; i++)
+            {
+                VertexArray line = new VertexArray(PrimitiveType.Lines, 2);
+                line[0] = new Vertex(new Vector2f
+                (
+                    Map(i - 1, 0, noiseFactors.Size, 0, winSizeX),
+                    Map(noise[i - 1], GetMin(noise), GetMax(noise), -winSizeY * 0.25f, winSizeY * 0.25f) + winSizeY * 0.5f
+                ), Color.White);
+                line[1] = new Vertex(new Vector2f
+                (
+                    Map(i, 0, noiseFactors.Size, 0, winSizeX),
+                    Map(noise[i], GetMin(noise), GetMax(noise), -winSizeY * 0.25f, winSizeY * 0.25f) + winSizeY * 0.5f
+                ), Color.White);
+                window.Draw(line);
+            }
+
             window.Display();
             window.Clear(new Color(25, 25, 25));
         }
